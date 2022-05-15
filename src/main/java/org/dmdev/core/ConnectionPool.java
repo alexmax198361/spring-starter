d7198eeb4c529308c8e2c6628c4cdc421957b48b
Alexandr Sazanovich <bear83.dev@gmail.com>
2022-05-15 11:05:00 +0300

    Зависимости через сеттеры

--- src/main/java/org/dmdev/core/ConnectionPool.java
+++ src/main/java/org/dmdev/core/ConnectionPool.java
@@ -8,13 +8,15 @@ public class ConnectionPool {
     private final String databaseName;
     private final Integer poolSize;
     private final List<String> args;
-    private final Map<String, Object> properties;
-
+    private Map<String, Object> properties;
 
     public ConnectionPool(String databaseName, Integer poolSize, List<String> args, Map<String, Object> properties) {
         this.databaseName = databaseName;
         this.poolSize = poolSize;
         this.args = args;
+    }
+
+    public void setProperties(Map<String, Object> properties) {
         this.properties = properties;
     }
 
